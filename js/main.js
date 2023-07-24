aba5bee8f2c3183248efa226bb79b340c176d40b
kuro-ashiato <developerashiato@gmail.com>
2023-07-24 14:05:10 +0900

    javascript add

--- js/main.js
+++ js/main.js
@@ -25,3 +25,22 @@ function hideBasket() {
     basketEl.classList.remove("show");
 }
 // basket end
+
+// search start
+const headerEl = document.querySelector("header");
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+    headerEl.classList.add("searching");
+}
+function hideSearch() {
+    headerEl.classList.remove("searching");
+}
+// search end
