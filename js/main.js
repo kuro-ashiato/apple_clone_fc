9138b2303fa85b59635c1bbeed29d7e63518b2f4
kuro-ashiato <developerashiato@gmail.com>
2023-07-27 15:51:07 +0900

    내용추가

--- js/main.js
+++ js/main.js
@@ -42,7 +42,10 @@ const searchInputEl = searchWrapEl.querySelector("input");
 const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
 searchStarterEl.addEventListener("click", showSearch);
-searchCloserEl.addEventListener("click", hideSearch);
+searchCloserEl.addEventListener("click", function (event) {
+    event.stopPropagation();
+    hideSearch();
+});
 searchShadowEl.addEventListener("click", hideSearch);
 
 function showSearch() {
@@ -90,6 +93,7 @@ const menuStarterEl = document.querySelector("header .menu-starter");
 menuStarterEl.addEventListener("click", function () {
     if (headerEl.classList.contains("menuing")) {
         headerEl.classList.remove("menuing");
+        searchInputEl.value = "";
         playScroll();
     } else {
         headerEl.classList.add("menuing");
@@ -97,6 +101,27 @@ menuStarterEl.addEventListener("click", function () {
     }
 });
 // 헤드 메뉴 토글 end
+
+// mob 헤더 검색 start
+const searchTextFieldEl = document.querySelector("header .textfield");
+const searchCancelEl = document.querySelector("header .search-canceler");
+
+searchTextFieldEl.addEventListener("click", function () {
+    headerEl.classList.add("searching--mobile");
+    searchInputEl.focus();
+});
+searchCancelEl.addEventListener("click", function () {
+    headerEl.classList.remove("searching--mobile");
+});
+// mob 헤더 검색 end
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth <= 740) {
+        headerEl.classList.remove("searching");
+    } else {
+        headerEl.classList.remove("searching--mobile");
+    }
+});
 
 // 요소의 가시성 관찰 start
 const io = new IntersectionObserver(function (entries) {
